d1c8aa7f621e080b0c47800d93a536647bd56b52
JUNGJIN <ojj824@naver.com>
2022-09-07 03:34:03 +0900

    feat: refreh token handling

---
+++ src/api/MyPage.js
@@ -0,0 +1,1 @@
+import { getCookieToken } from "storage/Cookie";
--- src/components/Answer/AnotherAnswer.js
+++ src/components/Answer/AnotherAnswer.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { apis } from "api/api";
 
 function AnotherAnswer() {
-	return <div>AnotherAnswer</div>;
+	const [anotherAnswer, setAnotherAnswer] = useState();
+
+	useEffect(() => {
+		apis
+			.anotherAnswer(2)
+			.then((response) => setAnotherAnswer(response.data.data));
+	}, []);
+	return (
+		<div>
+			{anotherAnswer &&
+				anotherAnswer.map((answer, index) => (
+					<div key={index} className="w-full p-2 m-4 border-2 rounded-3xl">
+						<div>{answer.nickname}</div>
+						<div>{answer.content}</div>
+					</div>
+				))}
+		</div>
+	);
 }
 
 export default AnotherAnswer;
