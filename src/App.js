c8f3843d85bfc85e59ce1ea179c9b8c099f5a09c
zi-zzang <kjhwlgusdl@gmail.com>
2022-09-08 12:33:45 +0900

    만든 사람들 modal 추가
    만든 사람들 modal 추가

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import { Routes, Route } from "react-router-dom";
-import Interview from "pages/Interview";
-import Main from "pages/Main";
-import SelectTopic from "pages/SelectTopic";
-import Ending from "pages/Ending";
-import MyPage from "pages/MyPage";
+import { Routes, Route } from 'react-router-dom';
+import Interview from 'pages/Interview';
+import Main from 'pages/Main';
+import SelectTopic from 'pages/SelectTopic';
+import Ending from 'pages/Ending';
+import MyPage from 'pages/MyPage';
+import Programmers from 'pages/Programmers';
 
 function App() {
-	return (
-		<Routes>
-			<Route path="/" element={<Main />}></Route>
-			<Route path="selecttopic" element={<SelectTopic />}></Route>
-			<Route path="interview/:id" element={<Interview />}></Route>
-			<Route path="ending" element={<Ending />}></Route>
-			<Route path="mypage" element={<MyPage />}></Route>
-		</Routes>
-	);
+  return (
+    <Routes>
+      <Route path='/' element={<Main />}></Route>
+      <Route path='selecttopic' element={<SelectTopic />}></Route>
+      <Route path='interview/:id' element={<Interview />}></Route>
+      <Route path='ending' element={<Ending />}></Route>
+      <Route path='mypage' element={<MyPage />}></Route>
+      <Route path='programmers' element={<Programmers />}></Route>
+    </Routes>
+  );
 }
 
 export default App;
--- src/pages/Ending.js
+++ src/pages/Ending.js
@@ -1,31 +1,50 @@
-import Layout from "components/Layout/Layout";
-import { LongButton } from "components/Button/Button";
-import { useNavigate } from "react-router";
-import React from "react";
-
+import Layout from 'components/Layout/Layout';
+import { LongButton } from 'components/Button/Button';
+import { useNavigate } from 'react-router';
+import React, { useState } from 'react';
+import { ProgrammersModal } from 'components/Modal/Modal';
+import Programmers from './Programmers';
 function Ending() {
-	const navigate = useNavigate();
+  const navigate = useNavigate();
+  const [checkProgrammers, setCheckProgrammers] = useState(true);
+  const [AnotherModalVisible, setAnotherModalVisible] = useState(false);
 
-	return (
-		<Layout>
-			<div className="flex items-center h-full">
-				<div className="flex flex-col items-center w-full gap-10 h-5/6">
-					<div className="m-40 text-6xl font-bold">
-						당신의 <span className="text-[#4593FC]">앞날</span>을 응원합니다 :)
-					</div>
-					<LongButton
-						onClick={() => {
-							navigate("/");
-						}}
-					>
-						다른 문제 풀러 가기
-					</LongButton>
-					<LongButton> 내가 푼 / 찜한 문제 확인하기 </LongButton>
-					<LongButton> 만든 사람들 </LongButton>
-				</div>
-			</div>
-		</Layout>
-	);
+  const openCheckAnotherAnswer = () => {
+    setAnotherModalVisible(!AnotherModalVisible);
+  };
+  return (
+    <>
+      <Layout>
+        <div className='flex items-center h-full'>
+          <div className='flex flex-col items-center w-full gap-10 h-5/6'>
+            <div className='m-40 text-6xl font-bold'>
+              당신의 <span className='text-[#4593FC]'>앞날</span>을 응원합니다 :)
+            </div>
+            <LongButton
+              onClick={() => {
+                navigate('/');
+              }}
+            >
+              다른 문제 풀러 가기
+            </LongButton>
+            <LongButton> 내가 푼 / 찜한 문제 확인하기 </LongButton>
+            <LongButton
+              onClick={() => {
+                openCheckAnotherAnswer();
+              }}
+            >
+              만든 사람들
+            </LongButton>
+          </div>
+        </div>
+      </Layout>
+      {checkProgrammers && (
+        <ProgrammersModal visible={AnotherModalVisible} closable={true} maskClosable={true} onClose={openCheckAnotherAnswer}>
+          <Programmers />
+        </ProgrammersModal>
+      )}
+    </>
+  );
 }
 
 export default Ending;
